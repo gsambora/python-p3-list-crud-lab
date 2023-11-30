26e96f221c068a116f4a63d452ea9d342375be48
Hana Sambora <grace.sambora@gmail.com>
2023-11-30 11:48:05 -0600

    Complete lab

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,29 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return ["meow", "woof", "neigh", "caw"]
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(element)
+    return l
 
 def add_element_to_start_of_list(l, element):
-    return None
+    new_list = [element]
+    new_list.append(l)
+    return new_list
 
 def remove_element_from_end_of_list(l):
-    return None
+    return l[0:len(l)-1]
 
 def remove_element_from_start_of_list(l):
-    return None
+    return l[1:len(l)]
 
 def retrieve_first_element_from_list(l):
-    return None
+    return l[0]
 
 def retrieve_element_from_index(l, index):
-    return None
+    return l[index]
 
 def retrieve_last_element_from_list(l):
-    return None
+    return l[-1]
